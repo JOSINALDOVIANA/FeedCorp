7aec2ed6a78f4a5cac282e5688c525a28cfeee03
Marcus Vinícius da Silva e Silva <marksilvag7@gmail.com>
2022-08-08 11:38:27 -0300

    Usando useStyles primeira vez

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,9 +1,47 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import {AppBar, Box, Toolbar, Typography, Button, IconButton} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useNavigate } from 'react-router-dom';
+import './App.css';
+import useStyles  from './styles';
 
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
 
-function App() {
+export default function App() {
+  const navegar = useNavigate();
+  const classes = useStyles();
+
   return (
-    <h1>Inicial</h1>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="absolute">
+        <Toolbar className={classes.root}>
+          {/* <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+
+          <Typography variant="h6" component="div" className={classes.Typography}>
+            OPCLIENT
+          </Typography>
+
+          <Button onClick={()=>navegar("/login")} color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    <DrawerHeader/>
+      <h1>OLÁ PESSOAS</h1>
+    </Box>
   );
 }
-
-export default App;
