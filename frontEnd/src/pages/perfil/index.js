1e42ceab1b663cddcd73bc467901dcbd19874cd5
Marcus Vinícius da Silva e Silva <marksilvag7@gmail.com>
2022-08-09 10:06:11 -0300

    Update Perfil

--- frontEnd/src/pages/perfil/index.js
+++ frontEnd/src/pages/perfil/index.js
@@ -1,22 +1,13 @@
 import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import {useNavigate} from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { UseDados } from '../../routes';
+import {
+  Box, Toolbar, List,
+  CssBaseline, Typography, Divider, IconButton,
+  ListItem, ListItemButton, ListItemIcon, ListItemText
+} from '@mui/material';
+
+// import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import HomeIcon from '@mui/icons-material/Home';
 import ApartmentIcon from '@mui/icons-material/Apartment';
 import ArticleIcon from '@mui/icons-material/Article';
@@ -24,68 +15,97 @@ import QuizIcon from '@mui/icons-material/Quiz';
 import ChatIcon from '@mui/icons-material/Chat';
 import SettingsIcon from '@mui/icons-material/Settings';
 import LogoutIcon from '@mui/icons-material/Logout';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+
+import { styled, useTheme } from '@mui/material/styles';
+import { AppBar, Drawer, DrawerHeader } from './styles';
 
 import Inicio from './páginas/inicio';
 import Ferramentas from './páginas/ferramentas/index.js';
-import { UseDados } from '../../routes';
-import { useLocation } from 'react-router-dom';
-import { AppBar, Drawer, DrawerHeader } from './styles';
 
 
 export default function Perfil() {
   const theme = useTheme();
+
   const [open, setOpen] = React.useState(false);
+  const handleDrawerOpen = () => setOpen(true);
+  const handleDrawerClose = () => setOpen(false);
+
   const [values, setValues] = UseDados();
   const [page, setPage] = React.useState("Início");
-const navegar=useNavigate();
+  const navegar = useNavigate();
   const rota = useLocation();
+
   React.useEffect(() => {
     setValues(rota.state)
   }, [])
   console.log(values)
-  const handleDrawerOpen = () => setOpen(true);
-
-  const handleDrawerClose = () => setOpen(false);
 
 
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
+        {/* BARRA SUPERIOR */}
         <Toolbar>
-          {/* <IconButton
+          <IconButton
             color="inherit"
             aria-label="open drawer"
-            onClick={handleDrawerOpen}
+            onClick={open ? handleDrawerClose : handleDrawerOpen}
             edge="start"
             sx={{
               marginRight: 5,
-              ...(open && { display: 'none' }),
+              //...(open && { display: 'none' }),
             }}
           >
-            <MenuIcon />
-          </IconButton> */}
+            {open ? <ChevronLeftIcon /> : <MenuIcon />}
+          </IconButton>
+
           <Typography variant="h6" noWrap component="div">
             Mini variant drawer
           </Typography>
+
         </Toolbar>
       </AppBar>
+      {/* BARRA LATERAL */}
       <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
+        <DrawerHeader sx={{display: 'flex', justifyContent: 'center'}}>
+          <MenuIcon/>
+          <Typography
+            variant="h5"
+            noWrap
+            onClick={() => {
+              navegar('/')
+            }}
+            sx={{
+              //display: { xs: 'none', md: 'flex' },
+              cursor: 'pointer',
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
         </DrawerHeader>
-        <Divider />
+
         <List>
           {['Início', 'Unidades', 'Avaliações', 'Ferramentas', 'Mensagens', 'Configurações', 'Sair'].map((text, index) => (
-            <ListItem onClick={()=>{
-              if (text=="Sair") {
-                localStorage.clear();
-                navegar('/login');
-              }
-              setPage(text);
-            }} key={text} disablePadding sx={{ display: 'block' }}>
+            <ListItem
+              onClick={() => {
+                if (text == "Sair") {
+                  localStorage.clear();
+                  navegar('/login');
+                }
+                setPage(text);
+              }}
+              key={text}
+              disablePadding
+              sx={{ display: 'block' }}
+            >
               <ListItemButton
                 sx={{
                   minHeight: 48,
@@ -111,6 +131,7 @@ const navegar=useNavigate();
                 </ListItemIcon>
 
                 <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+
               </ListItemButton>
             </ListItem>
           ))}
@@ -120,10 +141,10 @@ const navegar=useNavigate();
 
 
       {/* PÁGINAS */}
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+      <Box component="main" sx={{ flexGrow: 1, pl: 3, pr: 1 }}>
         <DrawerHeader />
         {page === "Início" && <Inicio />}
-        {page == "Ferramentas" && <Ferramentas/>}
+        {page == "Ferramentas" && <Ferramentas />}
       </Box>
     </Box>
   );
