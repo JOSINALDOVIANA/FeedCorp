3ed345e67b81498d8dd5246272abd02a6588948b
JOSINALDOVIANA <31937829+JOSINALDOVIANA@users.noreply.github.com>
2022-08-14 23:27:11 -0300

    erros conflit

--- frontend/src/pages/cadastro/index.js
+++ frontend/src/pages/cadastro/index.js
@@ -1,5 +1,9 @@
 import * as React from 'react';
+<<<<<<< Updated upstream
 import { useTheme } from '@mui/material/styles';
+=======
+
+>>>>>>> Stashed changes
 import {useNavigate} from 'react-router-dom';
 
 export default function Cadastro(){
