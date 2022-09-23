ca2699f34584823cb4ae94c0595da63ccfd05d4d
JOSINALDOVIANA <31937829+JOSINALDOVIANA@users.noreply.github.com>
2022-09-22 21:07:34 -0300

    Update index.js

--- backend/src/controller/pulses/index.js
+++ backend/src/controller/pulses/index.js
@@ -38,38 +38,102 @@ export default{
         try {        
 
          if(!!id_user){
+          // pulsos que o usuario criou
           let  pulsesCreateUser=await conexao("pulses").where({"pulses.id_user":id_user})
-            // .join("pulse_question","pulse_question.id_pulse","=","pulses.id");
+          
+          // Pulsos direcionados ao usuario
+          let  pulsesDirectUser=await conexao("pulse_user").where({"pulse_user.id_user":id_user})
+          .join("pulses","pulse_user.id_pulse","=","pulses.id")
+          .select("pulses.*");
+          
+          // dados do usuario
+          let mydados=await conexao("users").where({"users.id":id_user})
+          .first()
+          
+          
+          let myUnit = await conexao("user_unit").join("units","units.id","=","user_unit.id_unit")
+          .where({"user_unit.id_user":id_user})
+          .select("units.*")
+          .first();
+
+          // pulsos da companhia
+          let pulsescompany=await conexao("pulse_company").where({"pulse_company.id_company":mydados.id_company})
+          .join("pulses","pulse_company.id_pulse","=","pulses.id")
+          .select("pulses.*");
+
+          let pulsesunit=[];
+          // pulsos da unidade
+            if(!!myUnit){
+              pulsesunit=await conexao("pulse_unity").where({"pulse_unity.id_unity":myUnit.id})
+              .join("pulses","pulse_unity.id_pulse","=","pulses.id")
+              .select("pulses.*");
+            }
+
+          pulsesDirectUser=[...pulsesDirectUser,...pulsescompany,...pulsesunit] // juntando todos
+          pulsesDirectUser=pulsesDirectUser.filter(item=>{return item.id_user != id_user}); // filtrando os que o usuario criou
+          pulsesDirectUser=pulsesDirectUser.filter((este, i) => pulsesDirectUser.indexOf(este) === i);//tirando duplicatas
+
           for (const index in pulsesCreateUser) {
             let questions=await conexao("pulse_question").where({"id_pulse":pulsesCreateUser[index].id});
             for (let index2 in questions) {
-                let users=await conexao("answer_user").where({"answer_user.id_question":questions[index2].id})
-                .join("users","users.id","=","answer_user.id_user");
-                questions[index2]={...questions[index2],users}; 
+                let users_resp=await conexao("answer_user").where({"answer_user.id_question":questions[index2].id})
+                .join("users","users.id","=","answer_user.id_user").join("images","users.id_image","=","images.id")
+                .select("users.*","images.url","answer_user.status","answer_user.answer");
+                questions[index2]={...questions[index2],users_resp}; 
             }
             pulsesCreateUser[index]={...pulsesCreateUser[index],questions}
           }
-            return res.json({pulsesCreateUser});
-           
+
+          for (const index in pulsesDirectUser) {
+            let questions=await conexao("pulse_question").where({"id_pulse":pulsesDirectUser[index].id});
+            for (let index2 in questions) {
+                let resp=await conexao("answer_user").where({"answer_user.id_question":questions[index2].id,"answer_user.id_user":id_user})                
+                .select("answer_user.status","answer_user.answer");
+                questions[index2]={...questions[index2],resp:resp[0]}; 
+            }
+            pulsesDirectUser[index]={...pulsesDirectUser[index],questions}
+          }
+
+         
+            return res.json({pulsesCreateUser,pulsesDirectUser});           
          }
 
-        //  if(!!users){
-        //     users=users.map(user=>({id_pulse:id,id_user:user}));
-        //     await conexao("pulpse_user").insert(users);
-        //  }
+         if(!!id_company){
+          let  pulsesCompany=await conexao("pulses").where({"pulses.id_company":id_company})
+            
+          for (const index in pulsesCompany) {
+            let questions=await conexao("pulse_question").where({"id_pulse":pulsesCompany[index].id});
+            for (let index2 in questions) {
+                let users=await conexao("answer_user").where({"answer_user.id_question":questions[index2].id})
+                .join("users","users.id","=","answer_user.id_user").join("images","users.id_image","=","images.id")
+                .select("users.*","images.url","answer_user.answer","answer_user.status");
+                questions[index2]={...questions[index2],users}; 
+            }
+            pulsesCompany[index]={...pulsesCompany[index],questions}
+          }
+            return res.json({pulsesCompany});           
+         }
 
-        //  if(!!company){
-        //     await conexao("pulse_company").insert({id_pulse:id,id_company:company})
-        //  }
+         if(!!id_unit){
+          let pulsesUnit=await conexao("pulse_unity")
+          .join("pulses","pulses.id","=","pulse_unity.id_pulse").where({"pulse_unity.id_unity":id_unit})
+          .select("pulses.*");
+          for (const index in pulsesUnit) {
+            let questions=await conexao("pulse_question").where({"id_pulse":pulsesUnit[index].id});
+            for (let index2 in questions) {
+                let users=await conexao("answer_user").where({"answer_user.id_question":questions[index2].id})
+                .join("users","users.id","=","answer_user.id_user").join("images","users.id_image","=","images.id")
+                .select("users.*","images.url","answer_user.answer","answer_user.status");
+                questions[index2]={...questions[index2],users}; 
+            }
+            pulsesUnit[index]={...pulsesUnit[index],questions}
+          }
+            return res.json({pulsesUnit});           
+         }         
+         
+       
 
-        //  if(!!units){
-        //     units=units.map(unit=>({id_pulse:id,id_unity:unit}));
-        //     await conexao("pulse_unity").insert(units);
-        //  }
-
-        //   return res.json({status:true,pulse:{
-        //     id_user,id_company,title,questions,users,company,units
-        //   }})
+        return res.json({status:false,mensage:"DADOS NAO LOCALIZADOS"})
         } catch (error) {
             console.log(error)
             return res.json({status:false,mensage:"error pulses=>insert"})
