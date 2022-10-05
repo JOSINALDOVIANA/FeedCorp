3c0844b4c7ee6eab50bb869cdb703ed5533d76db
JOSINALDOVIANA <31937829+JOSINALDOVIANA@users.noreply.github.com>
2022-10-05 13:48:07 -0300

    exclusão arquivos ja desnecessarios backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,50 +7,29 @@ import morganBody from 'morgan-body';
 import fs from "fs";
 import moment from 'moment';
 
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const log = fs.createWriteStream(
-    path.resolve(__dirname,'./logs',`express-${moment().format("YYYY-MM-DD")}.log`),{flags:"a"}
+  path.resolve(__dirname, './logs', `express-${moment().format("YYYY-MM-DD")}.log`), { flags: "a" }
 )
 
 const App = express();
 App.use(function (req, res, next) {
-   // console.log('Time:', Date.now());
-    next();
-  });
-  App.use(cors());
-  // morganBody(App,{noColors:true,stream:log})
-  // App.use('/files',express.static(path.resolve(__dirname,'..','tmp','uploads')))
-  
-  // App.use(cors({ exposedHeaders: ['Total_lojas_cliente', 'Total_categorias',
-  //  'Total_criticas_loja', 'Total_categorias','login','update_prop','login_loja','FeedbackTotais','sugestoesTotais',
-  // 'PNETotais'] }));
-  
-  
-  App.use(express.json());
-  App.use(routes);
-// App.use((req,res,next)=>{
-//     const error = new Error('você esta tentando acessar um recurso inexistente.');
-//     error.status=404;
-//     next(error);
-// });
-// App.use((error, req, res, next) => {
-//     console.log(error.code);
-//     res.status("500");
-//     return res;
-    
-// });
-
-//////////////
-//git add *
-//git commit -m "mensagem"
-//git push
-//////////////
-
+  // console.log('Time:', Date.now());
+  next();
+});
+App.use(cors());
+morganBody(App, { noColors: true, stream: log })
+App.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
+App.use(express.json());
+App.use(routes);
+App.use("*", (req, res, next) => {
+  res.send("<span>você esta tentando acessar um recurso inexistente</span>")
+  next();
+});
 var port = process.env.PORT || 3002;
-
 App.listen(port, () => {
-    //console.log(`servidor rodando na porta: ${port}`);
+  console.log(`servidor rodando na porta: ${port}`);
 });
