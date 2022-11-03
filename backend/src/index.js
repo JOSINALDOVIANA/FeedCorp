d90a4ba98548183251508ad7765cced3a2c5c464
JOSINALDOVIANA <31937829+JOSINALDOVIANA@users.noreply.github.com>
2022-11-03 14:52:08 -0300

    upload 03

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,17 +11,17 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const log = fs.createWriteStream(
-  path.resolve(__dirname, './logs', `express-${moment().format("YYYY-MM-DD")}.log`), { flags: "a" }
-)
+// const log = fs.createWriteStream(
+//   path.resolve(__dirname, './logs', `express-${moment().format("YYYY-MM-DD")}.log`), { flags: "a" }
+// )
 
 const App = express();
-App.use(function (req, res, next) {
-  // console.log('Time:', Date.now());
-  next();
-});
+// App.use(function (req, res, next) {
+//   // console.log('Time:', Date.now());
+//   next();
+// });
 App.use(cors());
-morganBody(App, { noColors: true, stream: log })
+// morganBody(App, { noColors: true, stream: log })
 App.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')))
 App.use(express.json());
 App.use(routes);
@@ -29,7 +29,7 @@ App.use("/*", (req, res, next) => {
  return  res.send("<span>você esta tentando acessar um recurso inexistente</span>")
   
 });
-var port = process.env.PORT || 3002;
+var port = 3001;
 App.listen(port, () => {
   console.log(`servidor rodando na porta: ${port}`);
 });
