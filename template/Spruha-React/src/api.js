092e892f0d2188ca428e1723121271a650a49ecc
JOSINALDOVIANA <31937829+JOSINALDOVIANA@users.noreply.github.com>
2022-11-03 20:01:04 -0300

    upload contabo

--- template/Spruha-React/src/api.js
+++ template/Spruha-React/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `http://54.152.239.102:3001/`
+    baseURL: `http://109.123.243.212:3001/`
    // https://api-opclient-back.herokuapp.com
 //    http://127.0.0.1:3001
 })
