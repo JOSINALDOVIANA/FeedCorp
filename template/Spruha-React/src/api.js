79a8772a8aa28376d0aa3afb37fa48a1c3dd30b6
JOSINALDOVIANA <31937829+JOSINALDOVIANA@users.noreply.github.com>
2022-11-03 08:11:06 -0300

    cloud  aws

--- template/Spruha-React/src/api.js
+++ template/Spruha-React/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `http://3.87.218.247:3001/`
+    baseURL: `http://54.152.239.102:3001/`
    // https://api-opclient-back.herokuapp.com
 //    http://127.0.0.1:3001
 })
